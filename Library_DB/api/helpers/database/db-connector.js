433a6cbfc3aeb099d289cf626871430d839f2a9a
Marcus <roycefum@oregonstate.edu>
2026-05-27 14:44:42 +0200

    Add .env support, update README with env config and Windows instructions

--- Library_DB/api/helpers/database/db-connector.js
+++ Library_DB/api/helpers/database/db-connector.js
@@ -1,13 +1,14 @@
 // Adapted from 340 React Starter Code
 // Source: https://github.com/osu-cs340-ecampus/react-starter-app/?tab=readme-ov-file#react-and-nodejs-assignment---connecting-to-a-mysql-database
 
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'read_renaissance',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
